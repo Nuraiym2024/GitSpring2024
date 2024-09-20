a3d79728b9d9b4ab2614b4fdd2ba2d5e0577a1a4
Nuraiym <nzakirkyzy@gmail.com>
2024-09-20 10:50:12 +0600

    String1/MakeTags

---
+++ src/main/java/codingbat/warmup1/string1/MakeTags.java
@@ -0,0 +1,9 @@
+package codingbat.warmup1.string1;
+
+public class MakeTags {
+
+    public String makeTags(String tag, String word) {
+        return "<"+tag+">"+word+"<" + "/" +tag+ ">";
+    }
+
+}
