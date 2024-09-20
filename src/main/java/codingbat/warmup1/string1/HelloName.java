b40b057a428b22d1280b57ccb1e8b26c2ad429a8
Nuraiym <nzakirkyzy@gmail.com>
2024-09-20 10:46:06 +0600

    Srting1/HelloName/MakeAbba

---
+++ src/main/java/codingbat/warmup1/string1/HelloName.java
@@ -0,0 +1,9 @@
+package codingbat.warmup1.string1;
+
+public class HelloName {
+
+    public String helloName(String name) {
+
+        return "Hello " + name + '!';
+    }
+}
---
+++ src/main/java/codingbat/warmup1/string1/MakeAbba.java
@@ -0,0 +1,9 @@
+package codingbat.warmup1.string1;
+
+public class MakeAbba {
+
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
+}
